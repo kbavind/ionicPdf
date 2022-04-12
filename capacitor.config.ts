a3644331bbad8a3c5dcfef5e7dd8ffbe3eaf7244
Bavind <100546278+kbavind@users.noreply.github.com>
2022-04-12 11:22:19 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicPdf',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
